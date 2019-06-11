a09e36b4c5804ba990a30e277e3d454284b280e6
Dheeraj Chaudhary <38909585+dheeraj-2000@users.noreply.github.com>
2019-06-11 11:42:23 +0530

    This program hold the database of whole phonebook directory

---
+++ Phonebook in Python/database.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  2 14:28:45 2017
+
+@author: SATYAM COMPUTERS
+"""
+
+telephone={  7355280251:['Dheeraj chaudhary','male',17],
+             9122476671:['Ashish ','female',65],
+             9739887479:['Baswa Kiran','female',16],
+             1552959487:['Derik','gay',20],
+             7901088559:['Vamshi','male',25]             
+             }
